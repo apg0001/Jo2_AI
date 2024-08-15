ad834e3370e6fb5eda53df65353941ee90276687
박기찬 <apg0001naver@gmail.com>
2024-08-15 22:42:10 +0900

    add .gitattributes

--- models.py
+++
@@ -1,9 +0,0 @@
-# models.py
-
-class ChatRequest:
-    def __init__(self, message):
-        self.message = message
-
-class ChatResponse:
-    def __init__(self, response):
-        self.response = response
