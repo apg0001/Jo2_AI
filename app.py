44b997cf46eed8df32d588a19ac80b97ef86d9b2
박기찬 <apg0001naver@gmail.com>
2024-08-27 23:59:50 +0900

    set ssl adhoc

--- app.py
+++ app.py
@@ -224,4 +224,4 @@ def assess_depression(total_score: int) -> str:
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, ssl_context='adhoc')
